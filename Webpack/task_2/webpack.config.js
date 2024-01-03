88f28d2bd45780a3dc0ae15ba3f17909a5a312d5
virtualblakewillis <bewillis101@gmail.com>
2024-01-03 13:40:31 -0600

    task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+/* Modify the webpack config to support adding images to the CSS. */
+module.exports = {
+  entry: {
+    main: './js/dashboard_main.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'url-loader',
+        options: {
+          // Images larger than 10 KB won’t be inlined
+          limit: 10 * 1024
+        }
+      }
+
+    ],
+  },
+};
