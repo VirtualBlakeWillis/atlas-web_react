5770fe0415ea6ac960915c56ec03cd464f73ae40
virtualblakewillis <bewillis101@gmail.com>
2024-01-03 14:53:56 -0600

    trying a fix

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
 
   },
   devServer: {
-    contentBase: {directory: path.resolve(__dirname, 'public'), },
+    contentBase: path.resolve(__dirname, 'public'),
     compress: true,
     port: 8564,
 
