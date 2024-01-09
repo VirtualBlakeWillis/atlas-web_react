a82f361a45da5f1c1a06c77cb7141fb6759f72e2
virtualblakewillis <bewillis101@gmail.com>
2024-01-09 00:45:53 -0600

    task 4 done

---
+++ react_props/task_4/dashboard/src/App/App.js
@@ -0,0 +1,30 @@
+import holberton_logo from '../assets/holberton_logo.jpeg';
+import './App.css';
+import React from 'react';
+import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js';
+import Login from '../Login/Login.js';
+import Notifications from '../Notifications/Notifications.js';
+import CourseList from '../CourseList/CourseList.js';
+import PropTypes from 'prop-types';
+
+const appDefaults = {
+  isLoggedIn: false,
+};
+
+const App = ({isLoggedIn}) => {
+  return (
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+App.defaultProps = appDefaults;
+
+export default App;
---
+++ react_props/task_4/dashboard/src/CourseListRow/CourseListRow.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propDefaults = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+  /* if isHeader == true AND textSecondCell == null */
+  if (isHeader) {
+    if (textSecondCell == null) {
+      return (
+        <tr>
+          <th colSpan="2">{textFirstCell}</th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr>
+          <th>{textFirstCell}</th>
+          <th>{textSecondCell}</th>
+        </tr>
+      );
+    }
+  }
+  return (
+    <tr>
+      <td>{textFirstCell}</td>
+      <td>{textSecondCell}</td>
+    </tr>
+  );
+};
+
+CourseListRow.defaultProps = propDefaults;
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.oneOf([null]),
+  ]),
+};
+
+export default CourseListRow;
