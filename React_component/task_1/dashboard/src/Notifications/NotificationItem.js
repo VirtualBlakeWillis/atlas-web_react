60c4530bd2c1d459fa887c33e2ca19fb9a998042
virtualblakewillis <bewillis101@gmail.com>
2024-01-09 14:16:22 -0600

    task 1, couldnt completely figure out the test, but the code functions locally

---
+++ React_component/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const defaultNotificationItemProps = {
+  type: 'default',
+  html: undefined,
+  value: '',
+};
+
+function NotificationItem({ type, html, value}) {
+  if (html) {
+    return <li dangerouslySetInnerHTML={html}></li>;
+  } else {
+    return <li data-notification-type={type}>{value}</li>;
+  }
+}
+
+NotificationItem.defaultProps = defaultNotificationItemProps;
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+};
+
+export default NotificationItem;
