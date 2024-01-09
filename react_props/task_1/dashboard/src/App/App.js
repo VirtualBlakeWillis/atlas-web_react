ad29ce55a137b0dc8f6e0ac448e2c16c164af1bd
virtualblakewillis <bewillis101@gmail.com>
2024-01-08 21:59:43 -0600

    task 1

---
+++ react_props/task_1/dashboard/src/App/App.js
@@ -0,0 +1,23 @@
+import holberton_logo from '../assets/holberton_logo.jpeg';
+import './App.css';
+import React from 'react';
+import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js';
+import Login from '../Login/Login.js';
+import Notifications from '../Notifications/Notifications.js';
+
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
